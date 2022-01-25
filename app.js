0a17927710da96407b3056b0c71ee4597ced1e7e
roynirmal <nroy000@gmail.com>
2022-01-25 20:03:20 +0100

    button with random tasks

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/standard/template.html'));
+  res.sendFile(path.join(__dirname+'/standard/landing.html'));
   //__dirname : It will resolve to your project folder.
 });
 console.log(path)
