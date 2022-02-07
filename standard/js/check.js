58309420f3e711bbf31c97f8f2726cd7e57614ef
David Maxwell <maxwelld90@gmail.com>
2022-02-07 16:49:57 +0000

    Changed check code to hide comments

--- standard/js/check.js
+++ standard/js/check.js
@@ -21,25 +21,25 @@ document.addEventListener("DOMContentLoaded", function(){
         viewedList = JSON.parse(viewedList);
     
         if (viewedList.includes(currentPage)) {
-            console.log("This task exists in the list!");
+            //console.log("This task exists in the list!");
     
             if (viewedList.indexOf(currentPage) == (viewedList.length - 1)) {
-                console.log("but it is the most recent, so we let this one go.");
+                //console.log("but it is the most recent, so we let this one go.");
             }
             else {
-                console.log('this is an older task that they have already done, so we need to kick them');
+                //console.log('this is an older task that they have already done, so we need to kick them');
                 window.location = taskPageMapping[viewedList[viewedList.length - 1]];
             }
         }
         else {
             viewedList.push(currentPage);
-            console.log("Added new item to list");
+            //console.log("Added new item to list");
     
             window.sessionStorage.setItem('viewedList', JSON.stringify(viewedList));
         }
     }
     else {
-        console.log("No list!");
+        //console.log("No list!");
     
         viewedList = [currentPage];
     
